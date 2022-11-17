75b7c56e8dba045b068a76604cf6bdcc2e466b62
ddyrnes <dev.dyrnes@gmail.com>
2022-11-17 17:17:25 +0100

    Updated card.css + api call

--- js/api.js
+++ js/api.js
@@ -1,16 +1,20 @@
-const plainUrl = `https://api.dd2.no/wp-json/wp/v2/posts?per_page=50&_embed&`;
+const plainUrl = `https://api.dd2.no/wp-json/wp/v2/posts?per_page=3&_embed&categories=17&`;
 const key = `ck_1fea3ffc8bc99a342906cb9de269c915b0367458`;
 const secret = `cs_2aa926822b33ef9731b67319cd7a99585def833b`;
 const url = `${plainUrl}consumer_key=${key}&consumer_secret=${secret}`;
+// Categories:
+// News = 19
+// Tutorials = 18
+// Reviews = 17
 
-const selectContainer = document.querySelector(".api-test");
+const selectContainer = document.querySelector(".cards-container");
 
 async function getPosts(url) {
   try {
     const response = await fetch(url);
     const posts = await response.json();
     for (let i = 0; i < posts.length; i++) {
-      if (i >= 3) {
+      if (i >= 5) {
         break;
       } else {
         let newDate = new Date(posts[i].date);
@@ -24,13 +28,25 @@ async function getPosts(url) {
         if (month < 10) {
           month = "0" + month;
         }
-        console.log(posts[i].id);
+        console.log(posts[i]._embedded);
+        console.log(posts[i].excerpt.rendered);
         selectContainer.innerHTML += `
-        <a href+"${posts[i].id}">
-        <h2>${posts[i].title.rendered}</h2>
+        <a href+"${posts[i].id}" class="card flex-col">
         <img src="${posts[i]._embedded["wp:featuredmedia"]["0"].source_url}" alt="">
-        <p class="${posts[i]._embedded["wp:term"][0][0].name}">${posts[i]._embedded["wp:term"][0][0].name}</p>
+        <div class="card-category ${posts[i]._embedded["wp:term"][0][0].name}"><p>${
+          posts[i]._embedded["wp:term"][0][0].name
+        }
+        </p></div>
+        <div class="card-text flex-col">
+        <h2>${posts[i].title.rendered}</h2>
+        <div class="card-short-summary">${posts[i].excerpt.rendered}</div>
+        <div class="card-info flex-row">
+        <div>
         <p>By ${posts[i]._embedded.author[0].name}</p><p> ${year + "-" + month + "-" + date}</p>
+        </div>
+        <div class="card-link">Read More</div>
+        </div>
+        </div>
         </a>
         `;
 
