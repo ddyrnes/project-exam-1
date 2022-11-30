e5576343bb1c888dcd3fb14136d217edd1c9d3e8
ddyrnes <dev.dyrnes@gmail.com>
2022-11-30 16:57:36 +0100

    Updated css

--- js/module/createSpecificArticle.js
+++ js/module/createSpecificArticle.js
@@ -14,11 +14,12 @@ export async function createSpecificArticle(data) {
     month = "0" + month;
   }
   articleHeader.innerHTML = `
+      <div class="category-title card-category flex-row ">
+        <div class="${data._embedded["wp:term"][0][0].name} category-line"></div>
+        <p class="${data._embedded["wp:term"][0][0].name}">${data._embedded["wp:term"][0][0].name}</p>
+        <div class="${data._embedded["wp:term"][0][0].name} category-line"></div>
+      </div>
       <h1>${data.title.rendered}</h1>
-        
-      <p>Category: </p>
-      <p>${data._embedded["wp:term"][0][0].name}</p>
-  </div>
       <p>By ${data._embedded.author[0].name} | ${year + "-" + month + "-" + date}</p>
       `;
   articleContent.innerHTML = data.content.rendered;
